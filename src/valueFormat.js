8bf0da3e66319e69d938fd92715d75301a2922ab
Mofei Zhu <13761509829@163.com>
2015-08-30 23:08:49 +0800

    support inline symbol, like \n **abc** => <strong>abc</strong> \n   => <code>abc</code>

--- src/valueFormat.js
+++ src/valueFormat.js
@@ -1,11 +1,27 @@
+import styles from './modeStyle.js';
+
+var inlineFormat = {
+    'strong': '\\*\\*(.+?.)\\*\\*',
+    'code': '\\`(.+?)\\`',
+    'dispatch': function (val) {
+        for (var i in this) {
+            if (i == 'dispatch') continue;
+            var reg = new RegExp(this[i], 'g');
+            var style = styles(i) ? ' style="' + styles(i) + '"' : '';
+            val = val.replace(reg, '<' + i + style + '>$1</' + i + '>');
+        }
+        return val;
+    }
+}
+
 var valueFormat = {
-    ul: function (val) {
+    'ul': function (val) {
         return val.replace(/^\*(\s)*/, '<ul><li>').replace(/(\s)*\n(\s)*(\*){0,1}/g, '</li><li>').replace(/$/, '</li></ul>')
     },
-    ol: function (val) {
+    'ol': function (val) {
         return val.replace(/^1\.(\s)*/, '<ol><li>').replace(/(\s)*\n(\s)*(\*){0,1}/g, '</li><li>').replace(/$/, '</li></ol>')
     },
-    default: function (val) {
+    'default': function (val) {
         var self = this;
         var reg = new RegExp('^' + self.activeDom.dataset.symbol);
         val = val.replace(reg, '');
@@ -13,9 +29,13 @@ var valueFormat = {
         val = val.replace(/\s/g, '&nbsp;');
         return val;
     },
-    dispatch: function (val, type) {
-        var fn = valueFormat[type] || valueFormat['default'];
-        return fn.call(this, val);
+    'dispatch': function (val, type) {
+        // do main format
+        var mainFormat = valueFormat[type] || valueFormat['default'];
+        var mainResult = mainFormat.call(this, val);
+        // do inline format
+        var inlimeResult = inlineFormat.dispatch(mainResult);
+        return inlimeResult;
     }
 }
 
